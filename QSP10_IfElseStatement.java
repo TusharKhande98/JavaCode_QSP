4019d640d319d4e6ae8a7db48c3cd4f6fe5938ce
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-05-23 19:08:16 +0530

    Add files via upload

---
+++ QSP10_IfElseStatement.java
@@ -0,0 +1,18 @@
+class QSP10_IfElseStatement {
+    public static void main(String[] args) {
+
+        int number = 149;
+
+        if (number % 2 == 0)
+        {
+            System.out.println("even number");
+        }
+
+        else
+        {
+            System.out.println("odd number");
+        }
+
+
+    }
+}
---
+++ QSP9_IfStatement.java
@@ -0,0 +1,13 @@
+class QSP9_IfStatement {
+    public static void main(String[] args) {
+
+        int number = 78;
+
+        // checks if number is less than 0
+        if (number < 0) {
+            System.out.println("The number is negative.");
+        }
+        System.out.println("Code compiled successfully");
+
+    }
+}
