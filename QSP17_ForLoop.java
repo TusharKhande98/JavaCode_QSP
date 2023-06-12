66f64b06bc73f6ff373207787c681d754d242cba
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-06-12 17:40:06 +0530

    Add files via upload

---
+++ QSP17_ForLoop.java
@@ -0,0 +1,11 @@
+public class QSP17_ForLoop {
+    public static void main(String[] args) {
+
+        for (int start = 0; start<5; start++)
+        {
+            System.out.println("Hello, For loop.");
+        }
+
+
+    }
+}
