bfd2aa3365316dfeaa5a9a6131d6a7c4555e6c0b
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-06-14 18:31:28 +0530

    Add files via upload

---
+++ QSP17_ForLoop_Assgn3.java
@@ -0,0 +1,12 @@
+public class QSP17_ForLoop_Assgn3 {
+    public static void main(String[] args) {
+        // Infinite loop in Do-While
+        int i = 0;
+        do
+        {
+            System.out.println("Infinite loop");
+            i++;
+        }
+        while (true);
+    }
+}
